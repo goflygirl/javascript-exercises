3877522d0d5adba7dfe55aeef6397a7fdfe9a218
Ines Repnik <ines.repnik@gmail.com>
2024-01-31 19:25:30 +0100

    Completed Ex 1 and 2. All test passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
+const repeatString = function(text, num) {
+    let output = "";
+    if(num < 0 ){
+        output = "ERROR";
+    }
+    else{
+        for(let i = 1; i <= num; i++ ){
+            output += text;
 
+        }
+    }
+
+    return output;
 };
+
+repeatString('hey',3);
+repeatString('hey', 10);
+repeatString('hey',1);
+repeatString('hey',0);
+repeatString('hey', -10);
 
 // Do not edit below this line
 module.exports = repeatString;
