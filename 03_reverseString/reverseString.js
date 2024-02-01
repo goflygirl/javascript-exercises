8de92608bd06b40a79bb168e528b3335ac76e3c5
Ines Repnik <ines.repnik@gmail.com>
2024-02-01 17:01:33 +0100

    Wrote the code for ex 3 that passed tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let output = text.split('').reverse().join('');
+return output;
 };
 
 // Do not edit below this line
